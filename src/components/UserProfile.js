7729fb55e922c4ad739e869ff17b6ce25c007a22
Deepak Mahapatra <deepakmahapatra047@gmail.com>
2022-11-09 12:15:24 +0530

    Y

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -2,8 +2,15 @@ import { useContext } from 'react'
 import React from 'react'
 import { UserContext } from "./App"
 
-const UserProfile = () =>{
+const UserProfile = () => {
+    const value = useContext(UserContext);
 
+    return (
+        <>
+            <h1 id='name'>{value.name}</h1>
+            <h1 id='age'>{value.age}</h1>
+        </>
+    )
 }
 
-export {UserProfile}
+export { UserProfile }
